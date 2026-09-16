7c5844a09a4714075200570275952c64ed11d996
isaac <isaacloff@gmail.com>
2026-09-17 00:03:05 +0100

    rotary & button & main menu pages done

--- sensor-state/src/app_state.rs
+++ sensor-state/src/app_state.rs
@@ -1,4 +1,4 @@
-use crate::{DrawTextCallback, SetFontCallback, current_time::EnvironmentData, display_settings::DisplaySettings, font_type::FontType, pages::Page, sensor_data::{SensorData, sensor_accumulator::SensorAccumulator}, settings::Settings};
+use crate::{DrawTextCallback, SetFontCallback, current_time::EnvironmentData, display_settings::DisplaySettings, font_type::FontType, pages::Page, push_number, sensor_data::{SensorData, sensor_accumulator::SensorAccumulator}, settings::Settings};
 
 pub struct AppState {
     pub current_page: Page,
@@ -27,33 +27,51 @@ impl AppState {
         environment_data: &EnvironmentData
     ) {
         let button_just_pressed = sensor_data.button && !self.last_button_state;
-
         let time_since_last_press = environment_data.current_time.wrapping_sub(self.last_button_press_time);
         
-        if button_just_pressed && time_since_last_press > 200 {
-            let selected_page = if self.current_page == Page::MainMenuPage { Page::RotaryPage } else { Page::MainMenuPage }; 
-            self.current_page = selected_page;
+        let mut counted_click = button_just_pressed && time_since_last_press > 200;
+
+        let mut changed_page = false;
+        if counted_click {
+            let new_page = match self.current_page {
+                Page::MainMenuPage => {
+                    if sensor_data.rotary == 0 {
+                        Some(Page::ButtonPage)
+                    } else { None }
+                },
+                Page::ButtonPage => {
+                    if sensor_data.rotary == 0 {
+                        Some(Page::RotaryPage)
+                    } else { None }
+                },
+                Page::RotaryPage => {
+                    if sensor_data.rotary == 0 {
+                        Some(Page::MainMenuPage)
+                    } else { None }
+                }
+            };
+
+            if let Some(new_page) = new_page {
+                self.current_page = new_page;
+                changed_page = true;
+            }
 
             self.last_button_press_time = environment_data.current_time;
         }
 
-        self.last_button_state = sensor_data.button;
-        
-        match (self.settings.keep_all, self.settings.track_all) {
-            (_, true) => {
-                // accumulates ALL new sensor data regardless if page
-                self.sensor_accumulator.track(sensor_data);
-            },
-            (true, false) => {
-                // accumulates only the new sensor data for the current page
-                self.sensor_accumulator.track_one(&self.current_page, sensor_data);
-            },
-            (false, false) => {
-                // accumulates only the new sensor data for the current page
-                // removing all other accumulated data
-                self.sensor_accumulator.keep_one(&self.current_page, sensor_data);
-            },
+        if changed_page {
+            counted_click = false;
         }
+
+
+        match (self.settings.keep_all(), self.settings.track_all()) {
+            (_, true) => self.sensor_accumulator.track(counted_click, sensor_data),
+            (true, false) => self.sensor_accumulator.track_one(counted_click, &self.current_page, sensor_data),
+            (false, false) => self.sensor_accumulator.keep_one(counted_click, &self.current_page, sensor_data),
+        }
+
+
+        self.last_button_state = sensor_data.button;
     }
 
     pub fn render(
@@ -64,18 +82,30 @@ impl AppState {
         set_font_cb: SetFontCallback,
     ) {
         let mut cursor = [0, 0];
-        
-        let new_font_height = set_font_cb(FontType::Header as u8);
-        let page_header = self.current_page.header();
-        draw_text_cb(cursor[0], cursor[1], page_header.as_ptr());
-        
-        cursor[1] += new_font_height as i16;
 
-        let new_font_height = set_font_cb(FontType::Contents as u8);
-        for text in self.current_page.render() {
-            if let Some(text) = text {
+        let header_font_height = set_font_cb(FontType::Header as u8);
+
+        let mut page_header = self.current_page.header();
+        let _ = page_header.push_str(" \0");
+        
+        let mut current_end_x = draw_text_cb(cursor[0], cursor[1], page_header.as_ptr());
+
+        let mut timer = heapless::String::<32>::new();        
+        let total_secs = environment_data.current_time / 1000;
+        unsafe { push_number::<32, 10>(&mut timer, total_secs); } 
+        let _ = timer.push_str("s \0");
+
+        let content_font_height = set_font_cb(FontType::Contents as u8);
+        current_end_x = draw_text_cb(current_end_x, cursor[1], timer.as_ptr());
+
+        draw_text_cb(current_end_x, cursor[1], "<\0".as_ptr());
+        
+        cursor[1] += header_font_height as i16;
+        for text in self.current_page.render(&self.sensor_accumulator) {
+            if let Some(mut text) = text {
+                let _  = text.push('\0');
                 draw_text_cb(cursor[0], cursor[1], text.as_ptr());
-                cursor[1] += new_font_height as i16;
+                cursor[1] += content_font_height as i16;
             }
         }
     }
--- sensor-state/src/pages/mod.rs
+++ sensor-state/src/pages/mod.rs
@@ -1,33 +1,36 @@
-use core::fmt::Write;
 use heapless::String;
 
+use crate::{push_number, sensor_data::sensor_accumulator::SensorAccumulator};
+
 #[derive(PartialEq)]
+#[repr(u8)]
 pub enum Page {
     MainMenuPage,
+    ButtonPage,
     RotaryPage
 }
 
 impl Page {
-pub const fn default() -> Self {
+    pub const fn default() -> Self {
         Self::MainMenuPage
     }
 
-    pub fn header(&self) -> String<128> {
+    pub fn header(&self) -> String<16> {
         let mut text = String::new();
 
         let header = match self {
             Page::MainMenuPage => "Main Menu",
+            Page::ButtonPage => "Button",
             Page::RotaryPage => "Rotary",
         };
 
-        text.push_str(header).unwrap();
-        text.push_str("\0").unwrap();
+        text.push_str(header);
 
         text
     }
 
-    pub fn render(&self) -> impl Iterator<Item = Option<String<128>>> {
-        let mut result: [Option<String<128>>; 7] = [const { None }; 7];
+    pub fn render(&self, sensor_accumulator: &SensorAccumulator) -> impl Iterator<Item = Option<String<16>>> {
+        let mut result: [Option<String<16>>; 8] = [const { None }; 8];
 
         match self {
             Page::MainMenuPage => {
@@ -36,27 +39,58 @@ pub const fn default() -> Self {
                 let mut text3 = String::new();
                 let mut text4 = String::new();
                 let mut text5 = String::new();
-                let mut text6 = String::new();
-                let mut text7 = String::new();
+                // let mut text6 = String::new();
+                // let mut text7 = String::new();
+                // let mut text8 = String::new();
         
-                write!(text1, "Rotary\0").unwrap();
-                write!(text2, "Sound\0").unwrap();
-                write!(text3, "Light\0").unwrap();
-                write!(text4, "Temperature\0").unwrap();
-                write!(text5, "Humidity\0").unwrap();
-                write!(text6, "Pressure\0").unwrap();
-                write!(text7, "Acceleration\0").unwrap();
+                let _ = text1.push_str("Button");
+                let _ = text2.push_str("Rotary");
+                let _ = text3.push_str("Sound");
+                let _ = text4.push_str("Light");
+                let _ = text5.push_str("Humidity");
+                // let _ = text6.push_str("Temperature\0");
+                // let _ = text7.push_str("Pressure\0");
+                // let _ = text8.push_str("Acceleration\0");
 
                 result[0] = Some(text1);
                 result[1] = Some(text2);
                 result[2] = Some(text3);
                 result[3] = Some(text4);
                 result[4] = Some(text5);
-                result[5] = Some(text6);
-                result[6] = Some(text7);
+                // result[5] = Some(text6);
+                // result[6] = Some(text7);
+                // result[7] = Some(text8);
 
             },
-            Page::RotaryPage => {},
+            Page::ButtonPage => {
+                let mut text1 = String::new();
+                let _ = text1.push_str("count: ");
+                
+                let count = sensor_accumulator.button.count;
+
+                assert!(count < 1_000);
+
+                // Safety
+                // panics
+                unsafe { push_number::<16, 4>(&mut text1, count) };
+                
+                result[0] = Some(text1);
+            },
+            Page::RotaryPage => {
+                let mut text1 = String::new();
+                let _ = text1.push_str("value: ");
+                
+                let value = sensor_accumulator.rotary.value;
+
+                assert!(value < 10_000);
+                assert!(value >= 0);
+
+                // Safety
+                // panics
+                unsafe { push_number::<16, 5>(&mut text1, value as u32) };
+                
+                result[0] = Some(text1);
+            },
         };
 
         result.into_iter()
