51a3ef89bafecc35316443e6427638023e149513
isaac <isaacloff@gmail.com>
2026-09-17 00:02:04 +0100

    better sensor tracking

--- sensor-state/src/sensor_data/sensor_accumulator/mod.rs
+++ sensor-state/src/sensor_data/sensor_accumulator/mod.rs
@@ -13,17 +13,55 @@ impl SensorAccumulator {
         Self {
             button: ButtonAccumulator { count: 0 },
             rotary: RotaryAccumulator { value: 0 }
+        }
     }
 
-    pub fn track(&mut self, _sensor_data: &SensorData) {
 
+    /// accumulates ALL new sensor data regardless if page
+    pub fn track(&mut self, counted_click: bool, sensor_data: &SensorData) {
+        self.button.track(counted_click, sensor_data);
+        self.rotary.track(counted_click, sensor_data);
+
+        // sensor_data.acceleration
+        // sensor_data.humidity
+        // sensor_data.light
+        // sensor_data.pressure
+        // sensor_data.rotary
+        // sensor_data.sound
+        // sensor_data.temperature
     }
 
-    pub fn track_one(&mut self, _current_page: &Page, _sensor_data: &SensorData) {
-
+    /// accumulates only the new sensor data for the current page
+    pub fn track_one(&mut self, counted_click: bool, current_page: &Page, sensor_data: &SensorData) {
+        match current_page {
+            Page::MainMenuPage => {},
+            Page::ButtonPage => {
+                self.button.track(counted_click, sensor_data);
+            },
+            Page::RotaryPage => {
+                self.rotary.track(counted_click, sensor_data);
+            }
+        }
     }
 
-    pub fn keep_one(&mut self, _current_page: &Page, _sensor_data: &SensorData) {
-
+    /// accumulates only the new sensor data for the current page
+    /// 
+    /// removing all other accumulated data
+    pub fn keep_one(&mut self, counted_click: bool, current_page: &Page, sensor_data: &SensorData) {
+        match current_page {
+            Page::MainMenuPage => {
+                self.button.clear();
+                self.rotary.clear();
+            },
+            Page::ButtonPage => {
+                self.button.track(counted_click, sensor_data);
+                self.rotary.clear();
+            },
+            Page::RotaryPage => {
+                self.rotary.track(counted_click, sensor_data);
+                self.rotary.value = sensor_data.rotary;
+                self.button.clear();
+            }
+        }
     }
 }
