282a1a2d8ed168e476ddb43c6fcf6c3099feb167
isaac <isaacloff@gmail.com>
2026-09-17 00:02:42 +0100

    push number function formatter

--- sensor-state/src/lib.rs
+++ sensor-state/src/lib.rs
@@ -9,6 +9,7 @@ pub mod display_settings;
 pub mod current_time;
 pub mod font_type;
 
+use heapless::String;
 use sensor_data::SensorData;
 use app_state::AppState;
 use display_settings::DisplaySettings;
@@ -63,5 +64,35 @@ pub extern "C" fn tick(
 fn panic(_info: &PanicInfo) -> ! {
     unsafe {
         panic_blink(); 
+    }
+}
+
+/// # Safety
+/// 
+/// `MAX_DIGIT_COUNT` must be the maximum number of digits `num` could be
+pub unsafe fn push_number<
+    const N: usize, 
+    const MAX_DIGIT_COUNT: usize,
+>(
+    text: &mut String<N>, 
+    mut num: u32,
+) {
+    if num == 0 {
+        let _ = text.push('0');
+        return;
+    }
+
+    let mut buf = [0u8; MAX_DIGIT_COUNT];
+    let mut i = 0;
+
+    while num > 0 {
+        buf[i] = b'0' + (num % 10) as u8;
+        num /= 10;
+        i += 1;
+    }
+
+    while i > 0 {
+        i -= 1;
+        let _ = text.push(buf[i] as char);
     }
 }
