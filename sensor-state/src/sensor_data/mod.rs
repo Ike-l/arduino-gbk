00b41eecab1fa9ccc0cd513485bcb7e8fb2ced39
isaac <isaacloff@gmail.com>
2026-09-24 18:21:56 +0100

    updated types in sensor data

--- sensor-state/src/sensor_data/mod.rs
+++ sensor-state/src/sensor_data/mod.rs
@@ -4,13 +4,15 @@ pub mod sensor_accumulator;
 #[derive(Clone, Copy)]
 pub struct SensorData {
     pub button: bool,
-    pub rotary: i16,
-    pub sound: i16,
-    pub light: i16,
 
-    pub temperature: i32, // Celsius
-    pub humidity: f32, 
-    pub pressure: f32, // Pascals
+    pub rotary: u16,
+    pub sound: u16,
+    pub light: u16,
 
-    pub acceleration: [f32; 3], // X, Y, Z
+    pub temperature: i16, // Celsius //  * 10
+
+    pub humidity: u16, // * 10
+    pub pressure: u16, // Pascals // * 100
+
+    pub acceleration: [i16; 3], // X, Y, Z // * 100
 }
