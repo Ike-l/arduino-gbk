39df56d764e3fffbb7ff9590ca405d31effd8c14
isaac <isaacloff@gmail.com>
2026-09-24 18:22:27 +0100

    humidity accumulator in sensors

--- sensor-state/src/sensor_data/sensor_accumulator/mod.rs
+++ sensor-state/src/sensor_data/sensor_accumulator/mod.rs
@@ -1,17 +1,19 @@
-use crate::{pages::Page, sensor_data::{SensorData, sensor_accumulator::{button_accumulator::ButtonAccumulator, light_accumulator::LightAccumulator, rotary_accumulator::RotaryAccumulator, sound_accumulator::SoundAccumulator, temperature_accumulator::TemperatureAccumulator}}};
+use crate::{pages::Page, sensor_data::{SensorData, sensor_accumulator::{button_accumulator::ButtonAccumulator, humidity_accumulator::HumidityAccumulator, light_accumulator::LightAccumulator, rotary_accumulator::RotaryAccumulator, sound_accumulator::SoundAccumulator, temperature_accumulator::TemperatureAccumulator}}};
 
 pub mod button_accumulator;
 pub mod rotary_accumulator;
 pub mod sound_accumulator;
 pub mod light_accumulator;
 pub mod temperature_accumulator;
+pub mod humidity_accumulator;
 
 pub struct SensorAccumulator {
     pub button: ButtonAccumulator,
     pub rotary: RotaryAccumulator,
     pub sound: SoundAccumulator,
     pub light: LightAccumulator,
-    pub temperature: TemperatureAccumulator
+    pub temperature: TemperatureAccumulator,
+    pub humidity: HumidityAccumulator,
 }
 
 impl SensorAccumulator {
@@ -22,6 +24,7 @@ impl SensorAccumulator {
             sound: SoundAccumulator::default(),
             light: LightAccumulator::default(),
             temperature: TemperatureAccumulator::default(),
+            humidity: HumidityAccumulator::default(),
         }
     }
 
@@ -33,32 +36,23 @@ impl SensorAccumulator {
         self.sound.track(counted_click, sensor_data);
         self.light.track(counted_click, sensor_data);
         self.temperature.track(counted_click, sensor_data);
+        self.humidity.track(counted_click, sensor_data);
 
         // sensor_data.acceleration
-        // sensor_data.humidity
         // sensor_data.pressure
     }
 
     /// accumulates only the new sensor data for the current page
     pub fn track_one(&mut self, counted_click: bool, current_page: &Page, sensor_data: &SensorData) {
         match current_page {
-            Page::MainMenuPage => {},
-            Page::ButtonPage => {
-                self.button.track(counted_click, sensor_data);
-            },
-            Page::RotaryPage => {
-                self.rotary.track(counted_click, sensor_data);
-            },
-            Page::SoundPage => {
-                self.sound.track(counted_click, sensor_data);
-            },
-            Page::LightPage => {
-                self.light.track(counted_click, sensor_data);
-            },
-            Page::TemperaturePage => {
-                self.temperature.track(counted_click, sensor_data);
-            },
-            Page::SettingsPage => {}
+            Page::MainMenuPage => (),
+            Page::ButtonPage => self.button.track(counted_click, sensor_data),
+            Page::RotaryPage => self.rotary.track(counted_click, sensor_data),
+            Page::SoundPage => self.sound.track(counted_click, sensor_data),
+            Page::LightPage => self.light.track(counted_click, sensor_data),
+            Page::TemperaturePage => self.temperature.track(counted_click, sensor_data),
+            Page::HumidityPage => self.humidity.track(counted_click, sensor_data),
+            Page::SettingsPage => ()
         }
     }
 
@@ -71,6 +65,7 @@ impl SensorAccumulator {
         if !matches!(current_page, Page::SoundPage)  { self.sound.clear(); }
         if !matches!(current_page, Page::LightPage)  { self.light.clear(); }
         if !matches!(current_page, Page::TemperaturePage) { self.temperature.clear(); }
+        if !matches!(current_page, Page::HumidityPage) { self.humidity.clear(); }
 
         self.track_one(counted_click, current_page, sensor_data);
     }
